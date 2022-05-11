064daf6a6bdf19bb70060e87775a413bd1e24d2d
shristivyas <77220498+shristivyas@users.noreply.github.com>
2022-05-11 20:16:26 +0530

    Update MyNFT.sol

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -1,25 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 
 contract MyNFT is ERC721URIStorage, Ownable {
+    
+    AggregatorV3Interface internal priceFeed;
+    
     using Counters for Counters.Counter;
+
+    uint256 public constant mintprice = 1000;
+
+    uint256 public totalsupply = 35;
 
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Code Eater", "CER") {}
+
+    constructor() ERC721("Code Eater", "CER") {
+        priceFeed = AggregatorV3Interface(0xA6E711eE81700D14da2a1B871C4b9002110a854E);
+    }
 
     function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256)
     {
+        require(totalsupply>0 ,"limit exceeded");
         _tokenIds.increment();
+        totalsupply -= 1;
 
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
         return newItemId;
+    }
+
+    function getlatestprice() public view returns(int256){
+        (, int256 answer , , , ) = priceFeed.latestRoundData();
+        return int256(answer*10**10); //in wei 18 decimal
+
+    }
+
+    function getPriceRate() public view returns (uint) {
+        (, int price,,,) = priceFeed.latestRoundData();
+        uint adjust_price = uint(price) * 1e10;
+        uint usd = mintprice * 1e18;
+        uint rate = (usd * 1e18) / adjust_price;
+        return rate;
     }
 }
